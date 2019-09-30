2323a28c71b7d43d83e1a2cf6c5297ef214424a2
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2019-09-30 18:01:46 +0700

    Moved something to somewhere... goodnight...

--- db.py
+++ db.py
@@ -9,8 +9,6 @@ from pypika import MySQLQuery as Query, Table, Field
 def get_db():
     if "db" not in g:
         cfg = {
-            "host": config.MYSQL_HOST,
-            "port": config.MYSQL_PORT,
             "user": config.MYSQL_USER,
             "password": config.MYSQL_PASS,
             "database": config.MYSQL_DATABASE,
